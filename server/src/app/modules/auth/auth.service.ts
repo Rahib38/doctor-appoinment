3f6c220d2a6383df8c81cd8df9f2d90bae99ba22
Nadimul Rahib <nadimulrahib38@gmail.com>
2026-01-16 22:40:39 +0600

    jwt

--- server/src/app/modules/auth/auth.service.ts
+++ server/src/app/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../../../../lib/prisma";
 import { UserStatus } from "../../../../prisma/generated/prisma/enums";
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
 
 const login = async (payload: { email: string; password: string }) => {
   const user = await prisma.user.findUniqueOrThrow({
@@ -17,6 +18,22 @@ const login = async (payload: { email: string; password: string }) => {
 
   if (!isCorrectPassword) {
     throw new Error("Password incorrect!");
+  }
+
+
+  const accessToken= jwt.sign({email:user.email,role:user.role},"abcd",{
+    algorithm:"HS256",
+    expiresIn:"1h"
+  })
+
+    const refreshToken= jwt.sign({email:user.email,role:user.role},"abcd",{
+    algorithm:"HS256",
+    expiresIn:"30d"
+  })
+
+  return{
+    accessToken,
+    refreshToken
   }
 };
 
